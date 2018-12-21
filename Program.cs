03aa5dfb1568e04dc67aacecaea1525bec00c081
schristianson <shellychristianson.1@gmail.com>
2018-12-21 11:15:48 -0600

    My First Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
